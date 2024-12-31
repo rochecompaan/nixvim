2c774c7ec51a869fa0e49bcbd0b9d11ca7d8ae3e
Roché Compaan <roche@upfrontsoftware.co.za>
2024-12-31 11:29:19 +0200

    chore: remove systems we don't use

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     , ...
     } @ inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems = [ "x86_64-linux" ];
 
       perSystem =
         { system
@@ -40,10 +40,6 @@
         in
         {
           checks = {
-            default = pkgs.nixvimLib.check.mkTestDerivationFromNvim {
-              inherit nvim;
-              name = "A nixvim configuration";
-            };
             pre-commit-check = pre-commit-hooks.lib.${system}.run {
               src = ./.;
               hooks = {
