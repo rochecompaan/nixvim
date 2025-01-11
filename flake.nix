b489dc619acf3a0d25f549c0b5a5b575e53e18a3
Roché Compaan <roche@upfrontsoftware.co.za>
2025-01-11 17:39:40 +0200

    chore: switch to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Elyth's NeoVim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "github:nix-community/nixvim/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     pre-commit-hooks = {
