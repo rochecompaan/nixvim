3ebc0b0f7617924f87cc077edb229cbcf8646cc1
Roché Compaan <roche@sixfeetup.com>
2025-03-23 08:04:21 +0200

    fix: deprecation warnings

--- config/plug/lsp/conform.nix
+++ config/plug/lsp/conform.nix
@@ -6,24 +6,24 @@
         lspFallback = true;
         timeoutMs = 500;
       };
-    };
-    notifyOnError = true;
-    formattersByFt = {
-      html = [ [ "prettierd" "prettier" ] ];
-      css = [ [ "prettierd" "prettier" ] ];
-      javascript = [ [ "prettierd" "prettier" ] ];
-      javascriptreact = [ [ "prettierd" "prettier" ] ];
-      typescript = [ [ "prettierd" "prettier" ] ];
-      typescriptreact = [ [ "prettierd" "prettier" ] ];
-      python = [ "black" ];
-      lua = [ "stylua" ];
-      nix = [ "nixpkgs-fmt" ];
-      markdown = [ [ "prettierd" "prettier" ] ];
-      yaml = [ "yamllint" "yamlfmt" ];
-      "*" = [ "codespell" ];
-      # Use the "_" filetype to run formatters on filetypes that don't
-      # have other formatters configured.
-      "_" = [ "trim_whitespace" ];
+      formatters_by_ft = {
+        html = [ [ "prettierd" "prettier" ] ];
+        css = [ [ "prettierd" "prettier" ] ];
+        javascript = [ [ "prettierd" "prettier" ] ];
+        javascriptreact = [ [ "prettierd" "prettier" ] ];
+        typescript = [ [ "prettierd" "prettier" ] ];
+        typescriptreact = [ [ "prettierd" "prettier" ] ];
+        python = [ "black" ];
+        lua = [ "stylua" ];
+        nix = [ "nixpkgs-fmt" ];
+        markdown = [ [ "prettierd" "prettier" ] ];
+        yaml = [ "yamllint" "yamlfmt" ];
+        "*" = [ "codespell" ];
+        # Use the "_" filetype to run formatters on filetypes that don't
+        # have other formatters configured.
+        "_" = [ "trim_whitespace" ];
+      };
+      notify_on_error = true;
     };
   };
 }
