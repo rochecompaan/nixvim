28d94dd71fa172aefea3702ec9f1952ae5938646
Roché Compaan <roche@upfrontsoftware.co.za>
2025-06-09 13:44:23 +0200

    chore: image plugin settings

--- config/plug/utils/image.nix
+++ config/plug/utils/image.nix
@@ -3,15 +3,22 @@
     enable = true;
     settings = {
       backend = "kitty";
-      max_height = 40;
+      markdown = {
+        only_render_image_at_cursor = true;
+        only_render_image_at_cursor_mode = "popup";
+        download_remote_images = true;
+        enabled = true;
+        floating_windows = true;
+      };
+      max_height = null;
       max_height_window_percentage = {
         __raw = "math.huge";
       };
-      max_width = 100;
+      max_width = null;
       max_width_window_percentage = {
         __raw = "math.huge";
       };
-      window_overlap_clear_enabled = true;
+      window_overlap_clear_enabled = false;
       window_overlap_clear_ft_ignore = [
         "cmp_menu"
         "cmp_docs"
