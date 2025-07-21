8c91cfa4a8bfd6071925aa238d7b739be74e8ac9
Roché Compaan <roche@upfrontsoftware.co.za>
2025-07-21 12:44:09 +0200

    chore(harpoon): launch menu with <leader><CR>

--- config/plug/utils/harpoon.nix
+++ config/plug/utils/harpoon.nix
@@ -6,7 +6,7 @@
 
   keymaps = [
     { mode = "n"; key = "<C-a>"; action.__raw = "function() require'harpoon':list():add() end"; }
-    { mode = "n"; key = "<leader>h"; action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end"; }
+    { mode = "n"; key = "<leader><CR>"; action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end"; }
     { mode = "n"; key = "<C-1>"; action.__raw = "function()require'harpoon':list():select(1) end"; }
     { mode = "n"; key = "<C-2>"; action.__raw = "function()require'harpoon':list():select(2) end"; }
     { mode = "n"; key = "<C-3>"; action.__raw = "function()require'harpoon':list():select(3) end"; }
