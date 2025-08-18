3f90d80c2d0a46e31c30e767476a6348fa92a0e6
Roché Compaan <roche@upfrontsoftware.co.za>
2025-08-18 09:25:07 +0200

    chore(treesitter): disable custom yaml injections

--- config/plug/treesitter/treesitter.nix
+++ config/plug/treesitter/treesitter.nix
@@ -10,22 +10,22 @@
     };
   };
 
-  extraFiles = {
-    "/queries/yaml/injections.scm" = {
-      text = ''
-        (block_mapping_pair
-          key: (flow_node) @key-name
-          value: (block_sequence
-            (block_sequence_item (flow_node) @cmd-val)
-            (block_sequence_item (flow_node) @arg-val)
-            (block_sequence_item (block_scalar) @injection.content)
-          )
-          (#eq? @key-name "command")
-          (#any-of? @cmd-val "/bin/sh" "/bin/bash" "sh")
-          (#eq? @arg-val "-c")
-          (#set! injection.language "bash")
-          (#set! injection.include-children))
-      '';
-    };
-  };
+  # extraFiles = {
+  #   "/queries/yaml/injections.scm" = {
+  #     text = ''
+  #       (block_mapping_pair
+  #         key: (flow_node) @key-name
+  #         value: (block_sequence
+  #           (block_sequence_item (flow_node) @cmd-val)
+  #           (block_sequence_item (flow_node) @arg-val)
+  #           (block_sequence_item (block_scalar) @injection.content)
+  #         )
+  #         (#eq? @key-name "command")
+  #         (#any-of? @cmd-val "/bin/sh" "/bin/bash" "sh")
+  #         (#eq? @arg-val "-c")
+  #         (#set! injection.language "bash")
+  #         (#set! injection.include-children))
+  #     '';
+  #   };
+  # };
 }
