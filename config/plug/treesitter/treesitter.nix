72764e5fafb469091249d973882f60d089c4e195
Roché Compaan <roche@upfrontsoftware.co.za>
2025-08-18 09:14:03 +0200

    fix(tree-sitter): fix query syntax for YAML injections

--- config/plug/treesitter/treesitter.nix
+++ config/plug/treesitter/treesitter.nix
@@ -14,10 +14,10 @@
     "/queries/yaml/injections.scm" = {
       text = ''
         (block_mapping_pair
-          key: (_ @key-name)
+          key: (flow_node) @key-name
           value: (block_sequence
-            (block_sequence_item (plain_scalar) @cmd-val)
-            (block_sequence_item (plain_scalar) @arg-val)
+            (block_sequence_item (flow_node) @cmd-val)
+            (block_sequence_item (flow_node) @arg-val)
             (block_sequence_item (block_scalar) @injection.content)
           )
           (#eq? @key-name "command")
