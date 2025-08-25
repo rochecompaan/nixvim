fdd8c1f907d602d24911229e8432cccd5341cffb
Roché Compaan <roche@upfrontsoftware.co.za>
2025-08-25 13:52:12 +0200

    chore(neogit): custom settings

--- config/plug/git/neogit.nix
+++ config/plug/git/neogit.nix
@@ -2,11 +2,45 @@
   plugins.neogit = {
     enable = true;
     settings = {
+      commit_popup = {
+        kind = "floating";
+      };
+      disable_builtin_notifications = true;
+      disable_commit_confirmation = true;
       integrations = {
         diffview = true;
       };
+      kind = "floating";
+      popup = {
+        kind = "floating";
+      };
+      preview_buffer = {
+        kind = "floating";
+      };
+      sections = {
+        recent = {
+          folded = true;
+        };
+        staged = {
+          folded = false;
+        };
+        stashes = {
+          folded = true;
+        };
+        unmerged = {
+          folded = true;
+        };
+        unpulled = {
+          folded = false;
+        };
+        unstaged = {
+          folded = false;
+        };
+        untracked = {
+          folded = false;
+        };
+      };
     };
-    # Optional configuration can be added here
   };
 
   keymaps = [
