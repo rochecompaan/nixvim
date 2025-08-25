15262114f6d8ad4060f6afb85d855366d8374c2a
Roché Compaan <roche@upfrontsoftware.co.za>
2025-08-25 16:50:25 +0200

    chore(neogit): disable popups

--- config/plug/git/neogit.nix
+++ config/plug/git/neogit.nix
@@ -2,20 +2,8 @@
   plugins.neogit = {
     enable = true;
     settings = {
-      commit_popup = {
-        kind = "floating";
-      };
-      disable_builtin_notifications = true;
-      disable_commit_confirmation = true;
       integrations = {
         diffview = true;
-      };
-      kind = "floating";
-      popup = {
-        kind = "floating";
-      };
-      preview_buffer = {
-        kind = "floating";
       };
     };
   };
