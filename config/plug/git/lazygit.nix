584d961db1ad5b50bcc266b4ba1a93cf53f19ca7
Roché Compaan <roche@upfrontsoftware.co.za>
2025-10-13 09:32:51 +0200

    feat(lazygit): set NVIM and NVIM_LISTEN_ADDRESS

--- config/plug/git/lazygit.nix
+++ config/plug/git/lazygit.nix
@@ -4,14 +4,35 @@
   };
 
   extraConfigLua = ''
-    require("telescope").load_extension("lazygit")
+    -- Ensure child processes (like LazyGit) can find the current Neovim instance
+    local function set_nvim_env()
+      local addr = vim.v.servername
+      if addr and #addr > 0 then
+        vim.env.NVIM_LISTEN_ADDRESS = addr
+        vim.env.NVIM = addr
+      end
+    end
+
+    -- Set it at startup for all future child processes
+    set_nvim_env()
+
+    -- Provide a wrapper command that guarantees env is set just before launch
+    vim.api.nvim_create_user_command("LazyGitWithEnv", function()
+      set_nvim_env()
+      vim.cmd("LazyGit")
+    end, {})
+
+    -- Load telescope-lazygit if available
+    pcall(function()
+      require("telescope").load_extension("lazygit")
+    end)
   '';
 
   keymaps = [
     {
       mode = "n";
       key = "<leader>gg";
-      action = "<cmd>LazyGit<CR>";
+      action = "<cmd>LazyGitWithEnv<CR>";
       options = {
         desc = "LazyGit (root dir)";
       };
